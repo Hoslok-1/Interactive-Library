61156e0258f9c2f580db3cc0ca884b7f591057bc
Hoslok-1 <anujhoslokdev@gmail.com>
2022-11-11 19:10:24 +0530

    books can now be added and viewed in the library

--- scripts.js
+++ scripts.js
@@ -7,7 +7,9 @@ document.getElementById('submitBtn').onclick = function addBookToLibrary(){
     const bookObj = new Book(book,page)
     myLibrary.push(bookObj)
     console.log(myLibrary)
+    displayBooks();
 }
+
 function Book(bookName,page)
 {
     this.bookName = bookName;
@@ -17,3 +19,32 @@ function Book(bookName,page)
     }
 }
 
+function displayBooks()
+{
+    arrLength = myLibrary.length -1
+    tempName = myLibrary[arrLength].bookName;
+    tempPage = myLibrary[arrLength].page;
+    switch(arrLength)
+    {
+        case 0:
+            document.getElementById('book1-name').textContent = tempName;
+            document.getElementById('book1-pages').textContent = tempPage;
+            break;
+        case 1:
+            document.getElementById('book2-name').textContent = tempName;
+            document.getElementById('book2-pages').textContent = tempPage;
+            break;
+        case 2:
+            document.getElementById('book3-name').textContent = tempName;
+            document.getElementById('book3-pages').textContent = tempPage;
+            break;
+        case 3:
+            document.getElementById('book4-name').textContent = tempName;
+            document.getElementById('book4-pages').textContent = tempPage;
+            break;
+        case 4:
+            document.getElementById('book5-name').textContent = tempName;
+            document.getElementById('book5-pages').textContent = tempPage;  
+    }
+
+}
