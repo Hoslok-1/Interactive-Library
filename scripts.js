5ed53628195eead50e34c806f66410bdb914941f
Hoslok-1 <anujhoslokdev@gmail.com>
2022-11-15 17:44:55 +0530

    re-did the code to add multiple books to the library, also added basic css

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function openForm()
 {
     document.getElementById("myForm").style.display = "block";
@@ -7,9 +8,11 @@ function closeForm()
     document.getElementById("myForm").style.display = "none";
 }
 
-title = document.getElementById('bookTitle');
-author = document.getElementById('bookAuthor');
-pages = document.getElementById('bookPages');
+
+let title = document.getElementById('bookTitle');
+let author = document.getElementById('bookAuthor');
+let pages = document.getElementById('bookPages');
+let grid = document.getElementById('grid-container');
 document.getElementById('submitBtn').addEventListener('click', addBookToLibrary)
 
 
@@ -23,13 +26,32 @@ function Book(title,author,pages)
 }
 function addBookToLibrary()
 {
-
     const bookObj = new Book(title.value,author.value,pages.value)
     myLibrary.push(bookObj);
     console.log(myLibrary)
+    displayCards(bookObj);
+    closeForm();
 }
 
-function displayCards()
+function displayCards(book)
 {
-    
+    const gridItems = document.createElement('div')
+    const card = document.createElement('div')
+    const bookTitle = document.createElement('h5')
+    const bookAuthor = document.createElement('h6')
+    const pages = document.createElement('h6')
+
+    gridItems.classList.add("grid-item")
+    card.classList.add("card");
+
+    bookTitle.textContent = `Book Name:${book.title}`;
+    bookAuthor.textContent = `Book Author:${book.author}`;
+    pages.textContent = `Pages:${book.pages}`
+
+    grid.appendChild(gridItems);
+    gridItems.appendChild(card);
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(pages);
 }
+
